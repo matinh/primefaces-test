04b3ba6f36b99669a41990f6b87ad2bf93ea5dc0
matinh <martin.hoeller@xss.co.at>
2019-04-29 16:51:39 +0200

    adapted for <p:tabMenu> problem

--- src/main/java/org/primefaces/test/TestViewCdi.java
+++ src/main/java/org/primefaces/test/TestViewCdi.java
@@ -14,51 +14,27 @@ import java.util.List;
 @RequestScoped
 public class TestViewCdi {
 
-    private DualListModel<String> dualListModel;
-    private DualListModel<String> dualListModel2;
+    private String someInput = null;
+    private Integer activeTab = 0;
 
-    private String testString;
-    
-    @PostConstruct  
-    public void init() {
-        testString = "Welcome to PrimeFaces (from CDI!) !!!";
 
-        List<String> list1 = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e"));
-        List<String> list3 = new ArrayList<>();
-        dualListModel = new CollectionDualListModel<>(list1, list3);
-        dualListModel2 = new CollectionDualListModel<>(list1, list3);
-        System.out.println("TestViewCdi init() done.");
+    public String getSomeInput()
+    {
+        return someInput;
     }
 
-    public String getTestString() {
-        return testString;
+    public void setSomeInput(String someInput)
+    {
+        this.someInput = someInput;
     }
 
-    public void setTestString(String testString) {
-        this.testString = testString;
+    public Integer getActiveTab()
+    {
+        return activeTab;
     }
 
-    public DualListModel<String> getDualListModel() {
-        return dualListModel;
-    }
-
-    public void setDualListModel(DualListModel<String> dualListModel) {
-        this.dualListModel = dualListModel;
-    }
-
-    public DualListModel<String> getDualListModel2() {
-        return dualListModel2;
-    }
-
-    public void setDualListModel2(DualListModel<String> dualListModel2) {
-        this.dualListModel2 = dualListModel2;
-    }
-
-    public void action1() {
-        System.out.println("action1 called!");
-    }
-
-    public void action2() {
-        System.out.println("action2 called!");
+    public void setActiveTab(Integer activeTab)
+    {
+        this.activeTab = activeTab;
     }
 }
