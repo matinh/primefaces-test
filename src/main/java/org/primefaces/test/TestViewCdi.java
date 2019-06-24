b2db0a8074b9c65ec290d6d2272789e69400fb23
matinh <martin.hoeller@xss.co.at>
2019-06-24 16:36:31 +0200

    demonstrate problems with <p:dataGrid> and changing number of rows

--- src/main/java/org/primefaces/test/TestViewCdi.java
+++ src/main/java/org/primefaces/test/TestViewCdi.java
@@ -1,64 +1,38 @@
 package org.primefaces.test;
 
-import org.primefaces.model.DualListModel;
-
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-
-import java.util.ArrayList;
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
 @Named
-@RequestScoped
-public class TestViewCdi {
+@ViewScoped
+public class TestViewCdi implements Serializable
+{
+    private List<String> entries ;
+    private int columns = 1;
 
-    private DualListModel<String> dualListModel;
-    private DualListModel<String> dualListModel2;
-
-    private String testString;
-    
     @PostConstruct  
     public void init() {
-        testString = "Welcome to PrimeFaces (from CDI!) !!!";
-
-        List<String> list1 = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e"));
-        List<String> list3 = new ArrayList<>();
-        dualListModel = new CollectionDualListModel<>(list1, list3);
-        dualListModel2 = new CollectionDualListModel<>(list1, list3);
+        entries = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
         System.out.println("TestViewCdi init() done.");
     }
 
-    public String getTestString() {
-        return testString;
+    public List<String> getEntries()
+    {
+        return entries;
     }
 
-    public void setTestString(String testString) {
-        this.testString = testString;
+    public int getColumns()
+    {
+        return columns;
     }
 
-    public DualListModel<String> getDualListModel() {
-        return dualListModel;
-    }
-
-    public void setDualListModel(DualListModel<String> dualListModel) {
-        this.dualListModel = dualListModel;
-    }
-
-    public DualListModel<String> getDualListModel2() {
-        return dualListModel2;
-    }
-
-    public void setDualListModel2(DualListModel<String> dualListModel2) {
-        this.dualListModel2 = dualListModel2;
-    }
-
-    public void action1() {
-        System.out.println("action1 called!");
-    }
-
-    public void action2() {
-        System.out.println("action2 called!");
+    public void setColumns(int columns)
+    {
+        this.columns = columns;
     }
 }
