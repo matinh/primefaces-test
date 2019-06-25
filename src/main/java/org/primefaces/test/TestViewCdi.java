c6132366d1d30149095ea152c379313d721e8264
Martin Höller <martin.hoeller@xss.co.at>
2019-06-25 10:59:56 +0200

    reset to original PrimeFaces test

--- src/main/java/org/primefaces/test/TestViewCdi.java
+++ src/main/java/org/primefaces/test/TestViewCdi.java
@@ -2,48 +2,24 @@ package org.primefaces.test;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.List;
 
 @Named
-@ViewScoped
-public class TestViewCdi implements Serializable
-{
-    private List<String> entries ;
-    private int first = 0;
-    private int columns = 1;
+@RequestScoped
+public class TestViewCdi {
+
+    private String testString;
 
     @PostConstruct  
     public void init() {
-        entries = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
-        System.out.println("TestViewCdi init() done.");
+        testString = "Welcome to PrimeFaces (from CDI!) !!!";
     }
 
-    public List<String> getEntries()
-    {
-        return entries;
+    public String getTestString() {
+        return testString;
     }
 
-    public int getColumns()
-    {
-        return columns;
-    }
-
-    public void setColumns(int columns)
-    {
-        this.columns = columns;
-    }
-
-    public int getFirst()
-    {
-        return first;
-    }
-
-    public void setFirst(int first)
-    {
-        this.first = first;
+    public void setTestString(String testString) {
+        this.testString = testString;
     }
 }
